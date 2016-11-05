066c0277e012e02219b5a6709388efafad5026ba
SuperLaptop <t.totota@gmail.com>
2016-11-05 17:18:29 +0100

    no message

--- VRUnityProject/Assets/SpoonTraquer.cs
+++ VRUnityProject/Assets/SpoonTraquer.cs
@@ -6,14 +6,24 @@ public class SpoonTraquer : MonoBehaviour {
 	public GameObject spoon;
 	public GameObject attach;
 
+	public Transform content;
 
 	// Use this for initialization
 	IEnumerator Start () 
 	{
 		yield return new WaitForSeconds (2f);
+
+
+
+		spoon.transform.parent = attach.transform;
+		spoon.transform.localPosition = new Vector3(0F,-0.03F,0.12F);
+		spoon.transform.localEulerAngles = new Vector3 (0f, -90f, -12f);
+
+		spoon.transform.parent = null;
+
+
 		FixedJoint joint = spoon.AddComponent<FixedJoint>();
 		spoon.GetComponent<Rigidbody> ().velocity = Vector3.zero;
-		spoon.transform.position = attach.transform.position;
 		joint.connectedBody = attach.GetComponent<Rigidbody> ();
 	}
 	
