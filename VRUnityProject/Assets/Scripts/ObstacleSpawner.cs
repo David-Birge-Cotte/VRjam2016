b5b0758f8b0c79e79a0c8130652b6d4426e01dbc
prtesla <t.totota@gmail.com>
2016-11-05 18:06:10 +0100

    Merge

--- VRUnityProject/Assets/Scripts/ObstacleSpawner.cs
+++ VRUnityProject/Assets/Scripts/ObstacleSpawner.cs
@@ -55,7 +55,7 @@ public class ObstacleSpawner : MonoBehaviour {
                 //x /= 8; y /= 8;
                 int handDesignedSitu = Random.Range(0, handDesignedSituations.Count);
 
-                Vector3 spawnPos = transform.position + new Vector3(0, 0.5f, 0);
+                Vector3 spawnPos = transform.position + new Vector3(0, 0.25f, 0);
                 GameObject instantiatedObstacle = Instantiate(handDesignedSituations[handDesignedSitu], spawnPos, Quaternion.identity) as GameObject;
             }
             else
