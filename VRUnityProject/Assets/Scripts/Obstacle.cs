9db54c19f1806864584754cb9a3376342882c0ac
prtesla <t.totota@gmail.com>
2016-11-05 11:19:51 +0100

    Commit de trigger Obstacle

---
+++ VRUnityProject/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    public TriggerForObstacle[] Triggers;
+    public float speed = 1;
+
+    private Rigidbody _rb;
+
+	void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+	void Update ()
+    {
+        Move(-Vector3.right);
+    }
+
+    void Move(Vector3 movement)
+    {
+        _rb.MovePosition(transform.position + (movement * Time.deltaTime * speed));
+    }
+
+    public bool TestAllTriggers()
+    {
+        bool TestTrig = true;
+
+        for (int i = 0; i < Triggers.Length; i++)
+        {
+            if (Triggers[i].bIsTriggered == false)
+                TestTrig = false;
+        }
+
+        return TestTrig;
+    }
+}
---
+++ VRUnityProject/Assets/Scripts/TriggerForObstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerForObstacle : MonoBehaviour {
+
+    public bool bIsTriggered;
+
+    void OnTriggerEnter()
+    {
+        bIsTriggered = true;
+    }
+
+    void OnTriggerExit()
+    {
+        bIsTriggered = false;
+    }
+}
