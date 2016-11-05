2a8fd0f5f5a520e1e8b5e4ce9e48195bb77a75a5
SuperLaptop <t.totota@gmail.com>
2016-11-05 15:39:22 +0100

    spoon colision

---
+++ VRUnityProject/Assets/Editor/CheckPollySurface.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CheckPollySurface
+{
+	[MenuItem("Jam/Transform/Clean Surface")]
+	public static void CleanTransform()
+	{
+		Transform polysurface = (Selection.activeObject as GameObject).transform;
+
+		Debug.Log (polysurface);
+		Debug.Log (Selection.activeObject);
+
+		MeshRenderer[] meshes = polysurface.GetComponentsInChildren<MeshRenderer> ();
+
+		foreach (MeshRenderer mesh in meshes) 
+		{
+			Debug.Log ("mesh " + mesh.name);
+
+			MeshCollider collider = mesh.GetComponent<MeshCollider> ();
+
+			if (collider == null) 
+			{
+				collider = mesh.gameObject.AddComponent<MeshCollider> ();
+			}
+
+			try
+			{
+				collider.sharedMesh = mesh.GetComponent<MeshFilter>().sharedMesh;
+
+				Debug.Log(collider.sharedMesh.vertices.Length + " " + collider.sharedMesh.triangles.Length + " " + collider.sharedMesh.vertexCount);
+
+				if (collider.sharedMesh.vertices.Length >= 4)
+				{
+					collider.convex = true;
+				}
+				else
+				{
+					throw new System.Exception("not enough vertice " + collider.sharedMesh.vertexCount);
+				}
+			}
+			catch(System.Exception ex) 
+			{
+				Debug.LogWarning ("Failed to used surface " + mesh.name + " because " + ex.ToString ());
+				GameObject.Destroy(mesh.GetComponent<MeshCollider>());
+			}
+		}
+	}
+}
